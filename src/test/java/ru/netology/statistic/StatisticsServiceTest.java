72594d5f280319db3385956c4315dafbf70b7141
IbragimovaRoksana <roksalana91@mail.ru>
2020-12-14 20:36:14 +0500

    added OurTest

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,5 +17,17 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
 
+
+    }
+    @Test
+    void findMaxOwrMassive() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 34, 12};
+        long expected = 34;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
     }
 }
